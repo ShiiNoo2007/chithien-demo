954bb8697e63ec409f91b5fd78763294e05ff7bf
ShiiNoo2007 <149603803+ShiiNoo2007@users.noreply.github.com>
2024-10-15 14:16:52 +0700

    thêm
    dfsaasd

---
+++ thongdiep.js
@@ -0,0 +1,6 @@
+var x = 10;
+        var s = "ngôn ngữ lập trình javascipt";
+        document.write("<h1>giá trị của biến x là " + x + "</h1>");
+        console.log("giá trị của biến x là " + x);
+        var thongdiep = prompt("Hãy nhập thông điệp của bạn: ");
+        document.write(thongdiep);
